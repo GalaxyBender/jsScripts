7ef4de289ec68f6374e468901835578decec16c7
GalaxyBender <115903085+GalaxyBender@users.noreply.github.com>
2024-05-07 16:08:40 -0400

    Update x-frame-bypass.js

--- x-frame-bypass.js
+++ x-frame-bypass.js
@@ -84,7 +84,6 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
             'https://api.codetabs.com/v1/proxy/?quest=',
             'https://corsproxy.io/?',
 	    'https://api.allorigins.win/get?url=',
-	    '',
 	    'http://71.143.151.139:25565/',
 	    'http://71.143.151.139:8080/proxy/?url='
         ];
