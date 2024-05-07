c707ae018483d5d6d65cf65c2a142657cd9cdecc
GalaxyBender <115903085+GalaxyBender@users.noreply.github.com>
2024-05-07 15:48:50 -0400

    Update x-frame-bypass.js

--- x-frame-bypass.js
+++ x-frame-bypass.js
@@ -78,12 +78,13 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
     }
     fetchProxy(url, options, i) {
         const proxy = [
-            
             'https://cors.io?',
             'https://jsonp.afeld.me/?url=',
             'https://cors-anywhere.herokuapp.com/',
             'https://api.codetabs.com/v1/proxy/?quest=',
             'https://corsproxy.io/?',
+	    'https://api.allorigins.win/get?url=',
+	    '',
 	    'http://71.143.151.139:25565/',
 	    'http://71.143.151.139:8080/proxy/?url='
         ];
