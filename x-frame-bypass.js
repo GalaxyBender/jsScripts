204fa4fc72faff15bfa2a6453e046863c266c63c
GalaxyBender <115903085+GalaxyBender@users.noreply.github.com>
2024-05-08 11:15:44 -0400

    Update x-frame-bypass.js

--- x-frame-bypass.js
+++ x-frame-bypass.js
@@ -1,22 +1,18 @@
-
-
-
-
-
 customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.load(this.src);
+    this.src = '';
+    this.sandbox = '' + this.sandbox || 'allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation'; // all except allow-top-navigation
+  }
+  load(url, options) {
+    if (!url || !url.startsWith('http')) {
+      throw new Error(`X-Frame-Bypass src ${url} does not start with http(s)://`);
     }
-    connectedCallback() {
-        this.load(this.src);
-        this.src = '';
-        this.sandbox = '' + this.sandbox || 'allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation'; // all except allow-top-navigation
-    }
-    load(url, options) {
-        if (!url || !url.startsWith('http'))
-            throw new Error(`X-Frame-Bypass src ${url} does not start with http(s)://`);
-        console.log('X-Frame-Bypass loading:', url);
-        this.srcdoc = `<html>
+    console.log('X-Frame-Bypass loading:', url);
+    this.srcdoc = `<html>
 <head>
 	<style>
 	.loader {
@@ -44,9 +40,9 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	<div class="loader"></div>
 </body>
 </html>`;
-        this.fetchProxy(url, options, 0).then(res => res.text()).then(data => {
-            if (data)
-              	data = data.replace(/<head([^>]*)>/i, `<head$1>
+    this.fetchProxy(url, options, 0).then(res => res.text()).then(data => {
+      if (data) {
+        data = data.replace(/<head([^>]*)>/i, `<head$1>
 	<base href="${url}">
 	<script>
 	// X-Frame-Bypass navigation event handlers
@@ -67,43 +63,31 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
  
 	});
 	</script>`);
-                this.srcdoc = injectBase(data, url);
-}).catch(e => {
-  console.error('Cannot load X-Frame-Bypass:', e); 
-  alert('Cannot load X-Frame-Bypass: ' + e);
-});
-    }
-
-    injectBase(html, base) {
-        // Remove any <base> elements inside <head>     
-        html = html.replace(/(<[^>/]*head[^>]*>)[\s\S]*?(<[^>/]*base[^>]*>)[\s\S]*?(<[^>]*head[^>]*>)/img, "$1 $3");
-		
-        // Add <base> just before </head>  
-        html = html.replace(/(<[^>/]*head[^>]*>[\s\S]*?)(<[^>]*head[^>]*>)/img, `$1 ` + base + " $2");
-        return (html);
-    }
-    fetchProxy(url, options, i) {
-        const proxy = [
-	    'https://api.allorigins.win/get?url=',
-	    'https://api.codetabs.com/v1/proxy/?quest=',
-            'https://corsproxy.io/?',
-	    'https://cors-anywhere.herokuapp.com/',
-            'https://cors.io?',
-            'https://jsonp.afeld.me/?url=',
-	    'http://71.143.151.139:25565/',
-	    'http://71.143.151.139:8080/proxy/?url='
-        ];
-        return fetch(proxy[i] + url, options).then(res => {
-            if (!res.ok)
-                throw new Error(`${res.status} ${res.statusText}`);
-            return res;
-        }).catch(error => {
-            if (i === proxy.length - 1)
-                throw error;
-            console.log(proxy[i]);
-            return this.fetchProxy(url, options, i + 1);
-        });
-    }
+        this.srcdoc = this.injectBase(data, url);
+      }
+    }).catch(e => {
+      console.error('Cannot load X-Frame-Bypass:', e);
+      alert('Cannot load X-Frame-Bypass: ' + e);
+    });
+  }
+  injectBase(html, base) {
+    // Remove any <base> elements inside <head>     
+    html = html.replace(/(<[^>/]*head[^>]*>)[\s\S]*?(<[^>/]*base[^>]*>)[\s\S]*?(<[^>]*head[^>]*>)/img, "$1 $3");
+    // Add <base> just before </head>  
+    html = html.replace(/(<[^>/]*head[^>]*>[\s\S]*?)(<[^>]*head[^>]*>)/img, `$1 ` + base + " $2");
+    return (html);
+  }
+  fetchProxy(url, options, i) {
+    const proxy = ['https://api.allorigins.win/get?url=', 'https://api.codetabs.com/v1/proxy/?quest=', 'https://corsproxy.io/?', 'https://cors-anywhere.herokuapp.com/', 'https://cors.io?', 'https://jsonp.afeld.me/?url=', 'http://71.143.151.139:25565/', 'http://71.143.151.139:8080/proxy/?url='];
+    return fetch(proxy[i] + url, options).then(res => {
+      if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+      return res;
+    }).catch(error => {
+      if (i === proxy.length - 1) throw error;
+      console.log(proxy[i]);
+      return this.fetchProxy(url, options, i + 1);
+    });
+  }
 }, {
-    extends: 'iframe'
+  extends: 'iframe'
 });
